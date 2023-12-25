a86acb71d4c09871d82a9cf3b8732e85b2f9ab95
Farrel Sirah <farrelsirah@gmail.com>
2023-12-25 22:15:23 +0800

    sambil belajar git

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
-  <title>FS Blog | Home</title>
+  <title>FS Blog | About</title>
 </head>
 <body>
-  <h1>Halaman Home</h1>
-
-<script src="js/script.js"></script>
+  <h1>Halaman About</h1>
+  <h3>Farrel Sirah</h3>
+  <p>farrelsirah@gmail.com</p>
+  <img src="" alt="">
 </body>
 </html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,12 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>FS Blog | Home</title>
+  <title>FS Blog | Posts</title>
 </head>
 <body>
-  <h1>Halaman Home</h1>
-
-<script src="js/script.js"></script>
+  <h1>Halaman Posts</h1>
 </body>
 </html>
