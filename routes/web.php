1456ee26013c923db9d1f3f9cf2e6f0dd5b59d97
Farrel Sirah <farrelsirah@gmail.com>
2023-12-26 14:56:00 +0800

    belajar templating engine

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Farrel Sirah",
         "email" => "farrelsirah@gmail.com",
         "image" => "chika-fujiwara.jpg"
@@ -27,7 +30,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
 
 Route::get('/dashboard', function () {
