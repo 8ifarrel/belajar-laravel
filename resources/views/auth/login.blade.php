aeaf2de772cda7526c40eae8833e0ffa7ea34824
Farrel Sirah <farrelsirah@gmail.com>
2023-12-31 03:12:01 +0800

    fitur log in 40% selesai

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,49 +48,55 @@
 
 @extends('layouts.auth')
 
-@section('body')
-  <main class="m-auto">
-        <form>
-            <h3 class="mb-4">Log In</h3>
-        
-            <!-- Email -->
-            <div>
-                <x-input-label for="Log in" :value="__('Email address')" />
-                <x-text-input id="Log in" class="block form-control" type="Log in" name="Log in" :value="old('Log in')" required autofocus autocomplete="username"/>
-                <x-input-error :messages="$errors->get('Log in')" class="mt-2" />
-            </div>
+@section('container')
 
-            <!-- Password -->
-            <div>            
-                <x-input-label for="password" :value="__('Password')" />
-                <x-text-input id="password" class="block form-control" type="password" name="password" required autocomplete="current-password" placeholder=""/>
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-4">
+            <main class="m-auto">
+                <form>
+                <h2 class="mb-4">Log In</h2>
 
-            <!-- Forgot password -->
-            <div class="mb-3">
-                @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-            </div>
+                <!-- Email address -->
+                <div>
+                    <x-input-label for="Log in" :value="__('Email')" />
+                    <x-text-input id="Log in" class="block form-control border border-dark mt-2" type="Log in" name="Log in" :value="old('Log in')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('Log in')" class="mt-2" />
+                </div>
 
-            <div class="d-flex justify-content-between">
-                <!-- Remember me-->
-                <div class="form-check text-start block my-auto">
-                    <label for="remember_me" class="form-check-label">
-                        <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-                        <span>{{ __('Remember me') }}</span>
+                <!-- Password -->
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        @if (Route::has('password.request'))
+                        <a class="text-decoration-none" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                        @endif
+                    </div>
+
+                    <x-text-input id="password" class="block form-control border border-dark mt-2"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+            
+                <div class="form-check text-start my-3">
+                    <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                    Remember me
                     </label>
                 </div>
 
-                <!-- Log in -->
-                <button class="btn btn-primary" type="submit">Log in</button>
-            </div>
+                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
 
-            <!-- Register -->
-            <p class="mt-2 text-center">Not registered? <a href="/register">Register now!</a></p>
-        </form>
-    </main>
+                <p class="mt-3 text-center">
+                    Not registered? <a href="/register"> Register now!</a>
+                </p>
+                </form>
+            </main>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -14,17 +14,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 
-  <style>
-    html,
-    body {
-      height: 100%;
-    }
-    
-    .form-signin {
-      max-width: 330px;
-      padding: 1rem;
-    }
-
+  <style>    
     .form-signin .form-floating:focus-within {
       z-index: 2;
     }
@@ -42,7 +32,11 @@
     }
   </style>
 </head>
-<body class="d-flex justify-content-center">
-  @yield('body')
+<body>
+  <div class="container mt-5">
+    @yield('container')
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
